822ca5f92b7bb0b7cbfe9601958642c38b32a89d
Gato Gaiato <eduardofonteneledev@gmail.com>
2025-05-02 02:59:25 -0300

    bleh

--- configure.sh
+++ configure.sh
@@ -3,18 +3,18 @@
 USER_HOME="/home/$USER"
 
 if [ ! -d "$USER_HOME/Workspace" ]; then
-    mkdir $USER_HOME/Worskpace
+    mkdir $USER_HOME/Workspace
 fi
 
-
 if [ -f "$USER_HOME/.zshrc" ]; then
-    echo "alias >> zshrc"
-elif [ -f "$USER_HOME/.bashrc"]; then
-    echo "alias >> bashrc"
+    cat alias.txt >> $USER_HOME/.zshrc
+    echo "Aliases added to .zshrc"
+elif [ -f "$USER_HOME/.bashrc" ]; then
+    cat alias.txt >> $USER_HOME/.bashrc
+    echo "Aliases added to .bashrc"
 else
     echo "No file found. Bypassing"
 fi
-
 
 sudo apt install -y git
 sudo apt install -y maven
